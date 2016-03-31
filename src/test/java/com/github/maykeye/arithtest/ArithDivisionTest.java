93358b0571460c99ecd6436369efcc58037b7bbf
Maykeye <Maykeye@users.noreply.github.com>
2016-04-01 00:56:28 +0600

    Added test for division by zero

--- src/test/java/com/github/maykeye/arithtest/ArithDivisionTest.java
+++ src/test/java/com/github/maykeye/arithtest/ArithDivisionTest.java
@@ -6,6 +6,7 @@ import org.junit.runners.Parameterized;
 import ru.yandex.qatools.allure.annotations.Title;
 import java.util.Collection;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 @Title("Division test")
 @RunWith(Parameterized.class)
@@ -22,6 +23,7 @@ public class ArithDivisionTest extends ArithTest
 
     @Test
     public void testDivision(){
+        assertNotEquals("division by zero", 0, operand2);
         int actual = operand1 / operand2;
         assertEquals(expected, actual);
     }
