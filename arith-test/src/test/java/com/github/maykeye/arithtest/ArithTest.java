ebd526cbc6fce00e9f3734a470c2f0a17915bdb7
Maykeye <Maykeye@users.noreply.github.com>
2016-03-31 22:01:28 +0600

    Added parametrized test for +-*/
    Parametrized tests are described for JUnit4,
    so upgraded JUnit version in pom.xml

---
+++ arith-test/src/test/java/com/github/maykeye/arithtest/ArithTest.java
@@ -0,0 +1,65 @@
+package com.github.maykeye.arithtest;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * Unit test for simple App.
+ */
+@RunWith(Parameterized.class)
+public class ArithTest
+{
+    @Parameters(name="{index} -- ArithTest of {0} {2} {1} = {3}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                {0,0,"+",0},
+                {1,1,"/",1},
+                {2,1,"*",2},
+                {2,1,"??",2},
+                {2,0,"/",2},
+                {1,-1,"-",2},
+        });
+    }
+
+    private int operand1;
+    private int operand2;
+    private int expected;
+    private String operation;
+
+    public ArithTest(int operand1, int operand2, String operation, int expected){
+        this.operand1 = operand1;
+        this.operand2 = operand2;
+        this.operation = operation;
+        this.expected = expected;
+    }
+
+    @Test
+    public void implTest(){
+        int actual=0;
+
+        if ("+".equals(operation)){
+            actual = operand1+operand2;
+        } else if ("-".equals(operation)) {
+            actual = operand1-operand2;
+        } else if ("*".equals(operation)) {
+            actual = operand1*operand2;
+        } else if ("/".equals(operation)) {
+            actual = operand1/operand2;
+        } else {
+            fail(String.format("Unexpected operation %s", operation));
+        }
+
+        assertEquals(expected, actual);
+
+
+    }
+
+
+}
